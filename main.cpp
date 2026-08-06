8160f817ace82f08e2220f82abb7282e86b20872
Danya Bucenin <danyabucenin@gmail.com>
2026-08-07 00:24:43 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+
+int main()
+{
+    std::string string_to_guess = "aboba";
+    int attempts = 6;
+    char user_input = ' ';
+    while(//write your loop condition)
+    {
+        // main game loop
+        // get user input
+        // check if char in word
+        // open all chars if in word
+        // -- if char is not in word
+    }
+
+    return 0;
+}
